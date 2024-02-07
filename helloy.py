4f8c1ec5e67dbda8d5f9e7a0395346572fdc8794
Diego Rodrigues da Silva <dierodrigues@pagseguro.com>
2024-02-07 18:09:32 -0300

    Modificando o script hello

--- helloy.py
+++ helloy.py
@@ -1,2 +1,36 @@
-print("Hello, word".upper())
+"""Hello Word Multi Linguas.
+
+Dependendo da lingua configurada no ambiente o programa exibe a mensagem
+correspondente.
+
+Como usar:
+
+Tenha a variável LANG devidamente configurada ex:
+
+    export LANG=pt_BR
+
+Execução:
+  
+    python hello.py
+    ou
+    ./
+"""
+
+__version__ = "0.0.1"
+__author__ = "Diego Rocha"
+__license__ = "Unlicense"
+
+from os import getenv
+
+current_language = getenv("LANG")
+msg = "Hello, Word"
+
+if current_language == "pt_BR.UTF-8":
+    msg = "Iae, Mundo vei"
+elif current_language == "fn_FN":
+    msg = "Bonjour le monde"
+elif current_language == "al_AL":
+    msg = "Hallo Welt"
+
+print(msg.upper())
 
