214b1a9766293bc72f14472aa6496fda38c83f69
Diego Rodrigues da Silva <dierodrigues@pagseguro.com>
2025-01-11 19:25:55 -0300

    Add novos comentarios

--- teste.py
+++ teste.py
@@ -1,4 +1,10 @@
+#!/usr/bin/env python3
+
+# Is program print Hello World
+
 print("Vamos mandar um print da massa")
 msg = "Helloy word"
 
 print(msg.upper())
+
+# Add comentario em baixo para analizar no git diff
