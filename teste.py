1d2bafc2e54da578972a72b610088c45dd3a362e
Diego Rodrigues da Silva <dierodrigues@pagseguro.com>
2025-01-13 13:07:35 -0300

    Ajustando o primeiro script com variaveis de ambiente utilizando a lib os

--- teste.py
+++ teste.py
@@ -1,10 +1,22 @@
-#!/usr/bin/env python3
+"""Hello World Multi Linguas.
 
-# Is program print Hello World
+Dependendo da lingua configurada no ambiente o programa exibe a mensagem
+correspondente.
 
-print("Vamos mandar um print da massa")
+Tenha a variável LANG devidamente configurada ex:
+    export LANG=pt_BR
+
+Execução:
+    python3 hello.py
+"""
+
+from os import getenv
+
+currente_language = getenv("LANG")
+
 msg = "Helloy word"
 
-print(msg.upper())
+if currente_language == "pt_BR.UTF-8":
+    msg = "Olá, Mundo!"
 
-# Add comentario em baixo para analizar no git diff
+print(msg)
